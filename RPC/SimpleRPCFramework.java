c1a45de2490d6b67d7d5f57d160d58b7d472c229
wangjunlin <30399380+wangjunlin@users.noreply.github.com>
2019-03-19 11:47:34 +0800

    Update SimpleRPCFramework.java

--- RPC/SimpleRPCFramework.java
+++ RPC/SimpleRPCFramework.java
@@ -100,7 +100,7 @@ public class RPCFramework {
                             return result;
                         }
                     }
-                });
+                }).start();
         return proxy;
     }
 }
